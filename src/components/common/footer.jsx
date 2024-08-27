a780ed2e3dde23e72a4c2e8d14189bcb88eef624
Andrew Mao <maosteram@gmail.com>
2024-08-27 01:03:26 -0500

    Creates footer and project list view UI according to Figma design

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import "./footer.css";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="nav-footer">
+      <div className="nav-option">
+        <span className="circle-btn material-symbols-outlined">add</span>
+        <h3 className="nav-txt">Add Entry</h3>
+      </div>
+      <div className="nav-option">
+        <span className="circle-btn material-symbols-outlined">edit</span>
+        <h3 className="nav-txt">Edit Entry</h3>
+      </div>
+      <div className="nav-option">
+        <span className="circle-btn material-symbols-outlined">dashboard</span>
+        <h3 className="nav-txt">Dashboard</h3>
+      </div>
+    </div>
+  );
 };
--- src/components/projects/projects_list.jsx
+++ src/components/projects/projects_list.jsx
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./projects.css";
 
-const ProjectItem = (props) => {
+const ProjectItem = ({ project }) => {
+  const { id, title, description, startTime, duration } = project;
   return (
-    <div>
-      <h1>Project Item</h1>
+    <div className="project-item-container">
+      <div>
+        <h3 className="project-title">{title}</h3>
+        <h3 className="project-desc">{description}</h3>
+      </div>
+      <h3 className="divider-txt">00:00:00</h3>
     </div>
   );
 };
 
+// This weeks projects
+// Quick access tab of today's tasks
+// Secondary quick access to restart yesterday's tasks
+
 export const ProjectsList = () => {
-  return <div>Projects List</div>;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    chrome.storage.sync.get(["projects"]).then((results) => {
+      setProjects(results.projects);
+    });
+  }, []);
+
+  return (
+    <div className="projects-list">
+      <div className="timeframe-divider">
+        <h3 className="divider-txt">This Week</h3>
+        <h3 className="divider-txt">14 HR 38 MIN</h3>
+      </div>
+      <div className="timeframe-divider-sm">
+        <h3 className="divider-sm-txt">Today</h3>
+        <h3 className="divider-sm-txt">4 HR 30 MIN</h3>
+      </div>
+      {projects.map((project) => {
+        return <ProjectItem project={project}></ProjectItem>;
+      })}
+      <div className="timeframe-divider">
+        <h3 className="divider-txt">Yesterday</h3>
+        <h3 className="divider-txt">3 HR 18 MIN</h3>
+      </div>
+      {projects.map((project) => {
+        return <ProjectItem project={project}></ProjectItem>;
+      })}
+    </div>
+  );
 };
